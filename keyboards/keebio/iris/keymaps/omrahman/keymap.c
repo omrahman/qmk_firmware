fe2764246ddf7da4a51bcd2f02f1181312c73d25
Omar Rahman <orahman12@gmail.com>
2024-02-07 00:29:38 -0800

    Fill out SYM layer.

--- keyboards/keebio/iris/keymaps/omrahman/keymap.c
+++ keyboards/keebio/iris/keymaps/omrahman/keymap.c
@@ -96,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_BSPC, QHOME_A, QHOME_S, QHOME_D, QHOME_F, KC_G,                               KC_H,    QHOME_J, QHOME_K, QHOME_L, QHOME_SC,KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, QHOME_Z, KC_X,    KC_C,    QHOME_V, KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  QHOME_SL,KC_RSFT,
+     KC_LCTL, QHOME_Z, KC_X,    KC_C,    QHOME_V, KC_B,    KC_SLSH,          KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  QHOME_SL,KC_RGUI,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, KC_ESC,  KC_ENT,                    KC_SPC,  KC_BSPC, KC_RALT
+                                    KC_LGUI, KC_ESC,  KC_ENT,                    KC_SPC,  KC_BSPC, KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -110,23 +110,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_BSPC, CHOME_A, CHOME_R, CHOME_S, CHOME_T, KC_G,                               KC_M,    CHOME_N, CHOME_E, CHOME_I, CHOME_O, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, CHOME_Z, KC_X,    KC_C,    CHOME_D, KC_V,    KC_HOME,          KC_END,  KC_K,    KC_H,    KC_COMM, KC_DOT,  CHOME_SL,KC_RSFT,
+     KC_LCTL, CHOME_Z, KC_X,    KC_C,    CHOME_D, KC_V,    KC_SLSH,          KC_BSLS, KC_K,    KC_H,    KC_COMM, KC_DOT,  CHOME_SL,KC_RGUI,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, KC_ESC,  KC_ENT,                    KC_SPC,  KC_BSPC, KC_RALT
+                                    KC_LGUI, KC_ESC,  KC_ENT,                    KC_SPC,  KC_BSPC, KC_LALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [SYM] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     QK_BOOT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TO(ADJ),
+     _______, KC_GRV,  KC_LABK, KC_RABK, KC_AT,   KC_DOT,                             KC_AMPR, KC_BSLS, KC_LBRC, KC_RBRC, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL,  _______, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_PIPE,
+     _______, KC_EXLM, KC_PLUS, KC_MINS, KC_EQL,  KC_UNDS,                            KC_PIPE, KC_COLN, KC_LPRN, KC_RPRN, KC_PERC, TO(ADJ),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, _______, _______, _______, KC_DOWN, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
+     _______, _______, KC_CIRC, KC_ASTR, KC_HASH, _______, _______,          _______, KC_TILD, KC_DLR,  KC_LCBR, KC_RCBR, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, KC_DEL,                    KC_DEL,  _______, KC_P0
+                                    _______, _______, _______,                   _______,  _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
